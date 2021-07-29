9510324ae855d91485585fd24f7fbaff55ca6184
Kenny Lin <kenny@Kennys-MacBook-Pro.local>
2021-07-29 19:02:22 -0400

    loops 7/29

---
+++ loops/while_loops.py
@@ -0,0 +1,33 @@
+# while_loops.py
+# Purpose: learn how to use while loops
+
+# Loops in coding are used to execute the same lines of code over and over
+
+# the simplist of these is the while loop
+
+# while (condition):
+#     execute
+
+x = 0
+
+while x < 5: # will run until x >= 5
+    print(x)
+    x += 1 # same as x = x + 1
+print("loop completed\n")
+
+
+y = 0
+while y < 0:
+    print(y)
+    y += 1
+print("loop completed\n")
+
+
+# THIS IS AN INFINITE LOOP, BE CAREFUL
+# z = 0
+# while z >= 0:
+#     print(z)
+#     z += 1
+# print("loop completed")
+
+
