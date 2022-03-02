2e4da21c2c679e67293e4ada9e0f937e31c88948
Kenny Lin <kenny@kennys-mbp.myfiosgateway.com>
2022-03-02 18:35:05 -0500

    list practice

---
+++ lists/list_practice.py
@@ -0,0 +1,53 @@
+###############################
+# Sum list 
+# takes a list and returns the sum of all the elements in the list 
+# Ex: sumList([1, 2, 3]) returns 6
+###############################
+def sumList(arr):
+    pass # code here
+
+
+
+# Call the function with the following lists and print out the result
+test_1 = [1, 2, 3]
+test_2 = [1]
+test_3 = [-1, 0, 1]
+
+
+
+
+
+
+
+###############################
+# Find minimum
+# takes a list that is not empty and returns the minimum value from that list
+# Ex: findMin([1, 2, 3]) returns 1
+###############################
+def findMin(arr):
+    pass # code here 
+
+
+# Call the function with the following lists and print out the result 
+x = [1, 2, 3, 4, 5]
+y = [1]
+z = [-1, -2, -100]
+
+
+
+
+
+###############################
+# Find max
+# takes a list that is not empty and returns the maximum value from that list
+# Ex: findMin([1, 2, 3]) returns 3
+###############################
+def findMax(arr):
+    pass # code here 
+
+
+# Call the function with the following lists and print out the result 
+x = [1, 2, 3, 4, 5]
+y = [1]
+z = [-1, -2, -100]
+
