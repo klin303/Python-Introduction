b8df425eead3318a138c921fbc812b1edea35c80
Kenny Lin <kenny@kennys-mbp.myfiosgateway.com>
2022-03-30 18:20:19 -0400

    bubble sort

---
+++ lists/bubble_sort.py
@@ -0,0 +1,11 @@
+# Bubble Sort 
+
+# given a list in unsorted order, return the same list in sorted order 
+def bubbleSort(arr):
+    pass # CODE HERE 
+
+
+# test arrays 
+arr1 = [3, 2, 1]
+arr2 = [1, 4, 2, 3, 5]
+arr3 = [1, 2, 3]
